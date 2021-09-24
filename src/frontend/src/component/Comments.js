d6735771d2719bc7dc060d2e964deac3ab73041f
Egor Krasnonosenkikh <egor.krasnonosenkikh.mil@swf.army.mil>
2021-09-24 14:03:56 -0500

    MVP complete

---
+++ src/frontend/src/component/Comments.js
@@ -0,0 +1,8 @@
+import {useParams} from "react-router-dom";
+
+const CommentPage = ({}) => {
+    // const { name } = useParams();
+    return (<h1>test</h1>);
+}
+
+export default CommentPage;
---
+++ src/main/java/com/egor/radit/dto/RoleToUserDto.java
@@ -0,0 +1,2 @@
+package com.egor.radit.dto;public class RoleToUserDto {
+}
